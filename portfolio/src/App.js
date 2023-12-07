e3b7a050a78ef72ae5b1a76b31f6a3d7474908f5
Jacob <jakebeckstrom@gmail.com>
2023-12-07 08:53:06 -0600

    Adding beta link

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [projectData, updateProjectData] = React.useState({});
   const [view, updateView] = React.useState("home");
 
+  // const currentURL = window.location.href;
+  const currentURL = "https://jacob-beckstrom.com/";
   React.useEffect(() => {
     fetch("/static/resume_data.json", {
       headers: {
@@ -41,6 +43,26 @@ function App() {
   return (
     <main class="gradient-custom">
       <NavBar updateView={updateView} />
+      <div class="row text-center">
+        {currentURL === "https://jacob-beckstrom.com/" && (
+          <p p-2 m-2>
+            You are currently viewing the official version of this webiste.
+            Check out{" "}
+            <a href="http://beckstrom-portfolio-site-stage.s3-website.us-east-2.amazonaws.com">
+              this link
+            </a>{" "}
+            to see the beta version with the newest features.
+          </p>
+        )}
+        {currentURL ===
+          "http://beckstrom-portfolio-site-stage.s3-website.us-east-2.amazonaws.com/" && (
+          <p p-2 m-2>
+            You are currently viewing the beta version of this webiste. Check
+            out <a href="https://jacob-beckstrom.com">this link</a> to see the
+            official version with only stable features.
+          </p>
+        )}
+      </div>
       {view === "home" && <Home resume={resumeData} />}
       {view === "projects" && <Projects projects={projectData} />}
     </main>
